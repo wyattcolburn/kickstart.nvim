b07799fc99180bb3666e1af3fc16abcf91fe63e2
wyattcolburn <wyattdcolburn@gmail.com>
2024-08-23 18:31:21 -0700

    seeing how this works

--- init.lua
+++ init.lua
@@ -494,7 +494,7 @@ require('lazy').setup({
       --
       -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
       -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
+      -- ading my own comement for git
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
